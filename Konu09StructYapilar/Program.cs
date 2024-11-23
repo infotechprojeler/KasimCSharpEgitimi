ee97c18b858af08618a8dfb7f201db97b03b1d5d
InfotechProjeler <ilica-18@hotmail.com>
2024-11-23 10:53:31 +0300

    23-11-2024

--- Konu09StructYapilar/Program.cs
+++ Konu09StructYapilar/Program.cs
@@ -18,9 +18,45 @@
 
     internal class Program
     {
+        struct Kimlik
+        {
+            // değişkenler
+            public string Ad;
+            public string Soyad;
+            public int yas;
+            public string dogumYeri;
+
+            public struct Adres
+            {
+                // property ler
+                public int Id { get; set; }
+                public string Sehir { get; set; }
+                public string Mahalle { get; set; }
+                public string AcikAdres { get; set; }
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Konu 09 Struct-Yapilar!");
+
+            yapi ilkyapi = new yapi();
+            ilkyapi.metin = "yapı metni";
+            ilkyapi.sayi = 25;
+            ilkyapi.Metot();
+
+            Kimlik kimlik = new Kimlik();
+            kimlik.Ad = "Duygu";
+            kimlik.Soyad = "Kır";
+            kimlik.yas = 20;
+
+            Console.WriteLine($"Kişi Bilgileri : Ad {kimlik.Ad} - Soyad : {kimlik.Soyad}");
+
+            Kimlik.Adres adres = new();
+            adres.Sehir = "İstanbul";
+            adres.Mahalle = "Örnek";
+
+            Console.WriteLine(adres.Sehir);
+
         }
     }
 }
---
+++ Konu10StringSinifi/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Konu10StringSinifi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String Sınıfı!");
+            string degisken;
+            char karakter;
+            string metin = "String yan yana dizi formatında char yapısıdır.";
+            Ornek();
+        }
+        static void Ornek()
+        {
+            string birMetin = "Ankara başkenttir";
+            String birSayi = "123456789";
+            System.String birTarih = "23-11-2024";
+            string s = "Barış Manço";
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine($"s[{i}] {s[i]}"); // i nin değerini ve s değişkeninde i. indexteki değeri ekrana yaz
+            }
+            Console.WriteLine();
+            foreach (char c in s)
+            {
+                Console.WriteLine(c); // s değişkenindeki her karakteri tek tek ekrana yaz
+            }
+            Console.WriteLine("birMetin veri tipi : " + birMetin.GetType()); // GetType metodu bize ilgili değişkenin veri tipini getirir.
+            Console.WriteLine("birSayi veri tipi : " + birSayi.GetType());
+            Console.WriteLine("birTarih veri tipi : " + birTarih.GetType());
+        }
+    }
+}
